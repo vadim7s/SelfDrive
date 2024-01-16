57dd44281a1892a5680c173fc7507c7f701875e5
vadim7s <shumilov7@gmail.com>
2024-01-17 08:12:27 +1100

    RL steering and throttle

---
+++ RL1/test.py
@@ -0,0 +1,24 @@
+# to test a model 
+from stable_baselines3 import PPO
+from environment import CarEnv
+
+#update here
+models_dir = "C:\\SelfDrive\\models\\1705357424"
+
+env = CarEnv()
+env.reset()
+
+#and update here
+model_path = f"{models_dir}/1000000.zip"
+model = PPO.load(model_path, env=env)
+
+episodes = 5
+
+for ep in range(episodes):
+    obs = env.reset()
+    done = False
+    while not done:
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        #env.render()
+        print(reward)
---
+++ RL1/train saved.py
@@ -0,0 +1,34 @@
+from stable_baselines3 import PPO 
+from typing import Callable
+import os
+from environment import CarEnv
+import time
+
+
+print('This is the start of training script which opens an existing model and continues to train it')
+print('setting folders for logs and models')
+models_dir = "C:/SelfDrive/models/1705265602"
+logdir = f"logs/{int(time.time())}/"
+
+if not os.path.exists(logdir):
+	os.makedirs(logdir)
+
+print('connecting to env..')
+
+env = CarEnv()
+
+env.reset()
+print('Env has been reset as part of launch')
+print('Env action space:',env.action_space)
+# point to where your model is saved
+model_path = f"{models_dir}/1000000"
+model = PPO.load(model_path, env=env)
+print('Model action space:',model.action_space)
+TIMESTEPS = 250_000 # how long is each training iteration - individual steps
+iters = 0
+while iters<1:  # how many training iterations you want
+	iters += 1
+	print('Iteration ', iters,' is to commence...')
+	model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO" )
+	print('Iteration ', iters,' has been trained')
+	model.save(f"{models_dir}/{TIMESTEPS*iters}")
